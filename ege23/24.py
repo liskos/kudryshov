3c8143d619bb0ed9fc0a1a37a22922732f52ed68
Evgen <keril2004.nevazno@yandex.ru>
2022-09-04 09:58:25 +0300

    6

---
+++ ege23/24.py
@@ -0,0 +1,10 @@
+def f(a, b):
+    if a == b:
+        return 1
+    if a > b:
+        return 0
+    if a % 10 != 9:
+        return f(a + 1, b) + f(a + 11, b)
+    return f(a + 1, b) + f(a + 10, b)
+
+print(f(26, 49))
