9dd88bfff5eaa1d023f7d1dbe150c3e771a7a5d8
Evgen <keril2004.nevazno@yandex.ru>
2023-04-06 16:03:35 +0300

    09090

---
+++ ege16/109.py
@@ -0,0 +1,13 @@
+from functools import lru_cache
+
+@lru_cache
+def f(a,b):
+    if a == b:
+        return 1
+    if a == 47:
+        return 0
+    if a > b :
+        return 0
+    return f(a+1,b) + f(a * 2,b)
+
+print(f(2,12) * f(12,50))
